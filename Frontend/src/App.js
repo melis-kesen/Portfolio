c48ff47f1f040d2dc1c73d24f2ed53ae6b991ef8
Melis <meliskesen@gmail.com>
2023-07-18 23:15:09 +0300

    folders are added.

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,5 +1,31 @@
-import "./styles.css";
 
+import React, { useState, useEffect } from "react";
+import Navbar from "./components/Navbar";
+import Preloader from "../src/components/Preloader";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate
+} from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTop";
+import "./App.css";
 export default function App() {
-  return <div className="App">Portfolio</div>;
+  const [load, upadateLoad] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      upadateLoad(false);
+    }, 1200);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return <Router>
+  <Preloader load={load} />
+  <div className="App" id={load ? "no-scroll" : "scroll"}>
+    <Navbar />
+    <ScrollToTop />
+  </div>
+</Router>;
 }
---
+++ Frontend/src/components/Navbar.js
@@ -0,0 +1,5 @@
+import React, { useState } from "react";
+
+export default function NavBar(){
+    return <div className="App">NavBar </div>
+}
